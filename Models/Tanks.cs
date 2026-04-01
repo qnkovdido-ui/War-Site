84803e05edc6e9072fb43483f63fecc6eabcd22a
Students <Students@DESKTOP-C0DHI8H>
2026-04-01 13:53:07 +0300

    add tanks view

--- Models/Tanks.cs
+++ Models/Tanks.cs
@@ -2,8 +2,6 @@
 {
     public class Tanks
     {
-       
-      
             public int Id { get; set; }
             public string? Brand { get; set; }
             public string? Model { get; set; }
